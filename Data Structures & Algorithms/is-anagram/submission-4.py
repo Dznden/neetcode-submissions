059c382a4b6f7ea8e4e3537697193b422f01bf08
Jayden X Du <161931251+Dznden@users.noreply.github.com>
2026-07-17 13:04:20 +1000

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # idea doesnt matter about location of letter, just need same number of occurrences
+        counts = {}
+        for letter in s:
+            counts[letter] = counts.get(letter, 0) + 1
+        print(counts)
+        for letter2 in t:
+            print(letter2)
+            counts[letter2] = counts.get(letter2, 0) -1 # if not key value pair then default 0
+        #     if (counts[letter2]<0): # has to all equal 0 in the end to be valid
+        #         return False
+        # else:
+        #     return True
+        print(counts)
+        allLetters = s+t
+        for letters in allLetters:
+            if counts[letters]!=0:
+                return False
+        return True
+
+            
+
+            
+        
